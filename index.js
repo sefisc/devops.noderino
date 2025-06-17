19a9a921ad1cedd88c61e9dc63abae0292564a33
Sebastian Fischer <sebastian.fischer-96@web.de>
2025-06-17 21:18:37 +0200

    first push

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 80
 const number = process.env.NUMBER || 0
 
 app.get(desiredPath, (req, res) => {
-    res.send(`<h1>Hello from ${desiredPath} number ${number}!</h1>`)
+    res.send(`<h1>Hello from SEB'S NODERINO ${desiredPath} number ${number}!</h1>`)
 })
 
 app.get("/healthcheck", (req, res) => {
